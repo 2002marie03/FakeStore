46f96a07244a3adb0e9061be0fda6ae590bd786b
Meryem MENQIR <m.menqir.pfe@b3gtech.com>
2024-03-28 10:30:01 +0000

    AuthLocalStorage IMplementation and Test 2

--- app/src/main/java/com/baben/apps/appformation3/core/app/AuthLocalStorage.kt
+++ app/src/main/java/com/baben/apps/appformation3/core/app/AuthLocalStorage.kt
@@ -3,25 +3,29 @@ package com.baben.apps.appformation3.core.app
 import android.content.Context
 import android.content.SharedPreferences
 
-import androidx.appcompat.app.AppCompatActivity
 
 class AuthLocalStorage(private val context: Context) {
 
     companion object {
         val PREF:String="MY_PREF"
+        val key:String="token"
     }
     private val  sharedPreferences: SharedPreferences =  context.getSharedPreferences(  PREF
         , Context.MODE_PRIVATE)
 
-
     fun saveToken(token: String) {
         val editor = sharedPreferences.edit()
-        editor.putString("token", token)
+        editor.putString(key, token)
         editor.apply()
     }
 
-    fun getSavedToken(): String {
-       return "hello"
+    fun getSavedToken(): String? {
+       return sharedPreferences.getString(key,null)
+    }
+
+    fun userisconnected(): Boolean {
+        val token = sharedPreferences.getString(key, null)
+        return token != null
     }
 
 
--- app/src/test/java/com/baben/apps/appformation3/core/app/AuthLocalStorageTest.kt
+++ app/src/test/java/com/baben/apps/appformation3/core/app/AuthLocalStorageTest.kt
@@ -10,6 +10,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
@@ -25,11 +26,19 @@ class AuthLocalStorageTest {
 
     @Mock
     private lateinit var sharedPreferences: SharedPreferences
+
+    @Mock
+    private lateinit var editor: SharedPreferences.Editor
     @Before
     fun setUp() {
-
         Mockito.`when`(context.getSharedPreferences(any(String::class.java), any(Int::class.java)))
             .thenReturn(sharedPreferences)
+
+        Mockito.`when`(sharedPreferences.edit())
+            .thenReturn(editor)
+//        Mockito.`when`(sharedPreferences.edit().putString(any(String::class.java), any(String::class.java)))
+//            .thenReturn(editor)
+
         sut = AuthLocalStorage(context)
 
     }
@@ -51,24 +60,43 @@ class AuthLocalStorageTest {
     fun saveTokenTest() {
         var token: String = "iduiduiful454"
         sut.saveToken(token)
-        assertNotNull(sharedPreferences)
+        val key = ArgumentCaptor.forClass(String::class.java)
+        val value = ArgumentCaptor.forClass(String::class.java)
+        Mockito.verify(sharedPreferences.edit()).putString(key.capture(),value.capture())
+//        assertEquals(token,value.value)
+//        assertEquals("token",key.value)
+        Mockito.verify(editor).apply()
+//        assertNotNull(editor)
+
     }
 
     @Test
     fun getTokenTest() {
-
-        assertEquals("hello", sut.getSavedToken())
+        val expectedToken = "my_saved_token"
+        val key = ArgumentCaptor.forClass(String::class.java)
+        Mockito.`when`(sharedPreferences.getString("token", null)).thenReturn(expectedToken)
+        val retrievedToken = sut.getSavedToken()
+        assertEquals(expectedToken, retrievedToken)
+        Mockito.verify(sharedPreferences.edit(), Mockito.times(0)).putString(anyString(), anyString())
     }
 
     @Test
     fun user_is_connectedTest() {
-
+        val expected =""
+        val key = ArgumentCaptor.forClass(String::class.java)
+        Mockito.`when`(sharedPreferences.getString("token",key.value )).thenReturn(expected)
+        val isConnected=sut.userisconnected()
+        assertTrue(isConnected)
     }
 
     @Test
     fun user_is_not_connectedTest() {
+        val expected =null
+        val captor = ArgumentCaptor.forClass(String::class.java)
+        Mockito.`when`(sharedPreferences.getString("token", captor.value)).thenReturn(expected)
+        val isConnected=sut.userisconnected()
+        assertFalse(isConnected)
 
     }
-
 
 }
